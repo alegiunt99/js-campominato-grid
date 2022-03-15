af466e95eaf489cbf2a839f5edfa5c6b29e96efb
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-15 15:24:13 +0100

    Ho creato una cella e l'ho inserita nel container

--- js/script.js
+++ js/script.js
@@ -1,2 +1,36 @@
 console.log('JS OK!');
 
+/* Creare una griglia di gioco quadrata, 
+in cui ogni cella contiene un numero tra quelli compresi in un range compreso tra 1 e 100
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+*/
+
+// Creo la griglia di 100 numeri
+
+// prendo il contenitore delle celle
+const container = document.querySelector('.container');
+
+// divido il contenitore in colonne e righe per creare le celle
+const column = 10;
+
+const row = 10;
+
+// calcolo il numero di celle totali
+const totalBoxes = column * row;
+
+// creo il collegamento alla funziome er creare una cella 
+const boxes = createBox();
+
+// creo una cella 
+function createBox(){
+    const item = document.createElement('div');
+    item.classList.add('cell');
+    return item;
+}
+
+
+
+
+boxes.classList.add('boxes');
+
+container.appendChild(boxes);
